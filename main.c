baa45028a6a670f5b6ab2a6223fc4f8363ce4028
Damask Talary-Brown <=>
2016-10-30 17:54:51 +0000

    Everything is pthreads and everything hurts Swap function mysteriously broken, will come back to this Not even attempting to partition columns yet This has less validation than you could ever imagine

--- main.c
+++ main.c
@@ -1,12 +1,25 @@
-#include <ctype.h>
+#include <assert.h>
 #include <math.h>
+#include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <sys/time.h>
 #include <time.h>
-#include <unistd.h>
 
+#define likely(x)       __builtin_expect((x),1)
+#define unlikely(x)     __builtin_expect((x),0)
+
+#define DUMMY_3x3 {1, 1, 1, 1, 3, 1, 1, 1, 1}
+#define DUMMY_4x4 {1, 1, 1, 1, 1, 4, 4, 1, 1, 4, 4, 1, 1, 1, 1, 1}
+
+typedef struct thread_work
+{
+    double** read_from;
+    double** write_to;
+    int start_row;
+    int finish_row;
+    int nrows;
+    int ncols;
+} thread_work_t;
 
 int within_precision(double **one, 
                     double **two, 
@@ -36,12 +49,12 @@ void relax_cell(double **in, double **out, int row, int col)
     out[row][col] = adjac/(double)4;
 }
 
-void swap(double ***one, double ***two)
-{
-    double **tmp = *one;
-    *one = *two;
-    *two = tmp;
-}
+// void swap(double ***one, double ***two)
+// {
+//     double **tmp = *one;
+//     *one = *two;
+//     *two = tmp;
+// }
 
 void populate_random(double **one, double **two, int nrows, int ncols) 
 {
@@ -50,8 +63,30 @@ void populate_random(double **one, double **two, int nrows, int ncols)
     {
         for (int j=0; j<ncols; ++j)
         {
-            one[i][j] = 10*(double)rand()/(double)RAND_MAX;
-            two[i][j] = one[i][j];
+            double r = 10*(double)rand()/(double)RAND_MAX;
+            one[i][j] = r;
+            two[i][j] = r;
+        }
+    }
+}
+
+void populate_values(double vals[]
+                    int nvals,
+                    double **one, 
+                    double **two, 
+                    int nrows, 
+                    int ncols)
+{
+    // Make sure we can populate the whole array from vals[]
+    assert(nvals == (nrows*ncols));
+    int v = 0;
+    for (int i=0; i<nrows; ++i)
+    {
+        for (int j=0; j<ncols; ++j)
+        {
+            one[i][j] = (double)vals[v];
+            two[i][j] = (double)vals[v];
+            ++v;
         }
     }
 }
@@ -69,18 +104,50 @@ void print_matrix(double **arr, int nrows, int ncols)
     printf("\n");
 }
 
+void *relax_section(void *work_ptr)
+{
+    thread_work_t *work = (thread_work_t*)work_ptr;
+    for (int i=work->start_row; i<=work->finish_row; ++i)
+    {
+        for (int j=1; j<work->ncols-1; ++j)
+        {
+            relax_cell(work->read_from, work->write_to, i, j);
+        }
+    }
+    return NULL;
+}
+
 int main(void)
 {
     int nrows = 7;
-    int ncols = nrows;
     double precision = (double)0.05;
+    int nthreads = 4;
+
+    printf("Enter array dimension: ");
+    scanf("%d", &nrows);
+
+    printf("Enter precision: ");
+    scanf("%lf", &precision);
+
+    printf("Enter number of threads: ");
+    scanf("%d", &nthreads);
+
+    int ncols = nrows;
+    int work_rows = (nrows-2)/nthreads;
+    int work_remainder = (nrows-2)%nthreads;
+    thread_work_t* all_work = malloc(nthreads*sizeof(thread_work_t));
+
+    if (unlikely(all_work==NULL))
+    {
+        return 1;
+    }
     
     double **from = malloc(nrows*sizeof(double*));
     double **to   = malloc(nrows*sizeof(double*));
     double *from_buf = malloc(nrows*ncols*sizeof(double));
     double *to_buf   = malloc(nrows*ncols*sizeof(double));
 
-    if (from==NULL || from_buf== NULL || to==NULL || to_buf== NULL)
+    if (unlikely(from==NULL || from_buf== NULL || to==NULL || to_buf== NULL))
     {
         return 1;
     }
@@ -93,24 +160,63 @@ int main(void)
 
     populate_random(from, to, nrows, ncols);
 
+    int next_start=1;
+    for (int t=0; t<nthreads; ++t)
+    {
+        thread_work_t work = 
+        {
+            .read_from = from,
+            .write_to = to,
+            .start_row = next_start,
+            .finish_row = next_start+work_rows-1 + (t<work_remainder ? 1:0),
+            .nrows = nrows,
+            .ncols = ncols
+        };
+        next_start = work.finish_row+1;
+        printf("Thread %d doing rows %d-%d\n", t, work.start_row, work.finish_row);
+        all_work[t] = work;
+    }
+
+
+    pthread_t threads[nthreads];
     int relaxed = 0, count = 0;
     while (!relaxed)
     {
-        for (int i=1; i<nrows-1; ++i)
-        {
-            for (int j=1; j<ncols-1; ++j)
+        for (int t=0; t<nthreads; ++t)
+        {   
+            if (count%2)
             {
-                relax_cell(from, to, i, j);
+                all_work[t].read_from = to;
+                all_work[t].write_to = from;
+            }
+            else
+            {
+                all_work[t].read_from = from;
+                all_work[t].write_to = to;
+            }
+            if (pthread_create(&threads[t], NULL, relax_section, &all_work[t]))
+            {
+                fprintf(stderr, "Failed creating thread %d\n", t);
+                return 1;
+            }
+        }
+        for (int t=0; t<nthreads; ++t)
+        {
+            if (pthread_join(threads[t], NULL))
+            {
+                fprintf(stderr, "Failed joining thread %d\n", t);
+                return 1;
             }
         }
         relaxed = within_precision(from, to, nrows, ncols, precision);
-        swap(&from, &to);
         ++count;
     }
+    printf("Reached in %d iterations.\n", count);
 
-    printf("Reached in: %d iterations.\n", count);
-    
     free(from);
+    free(to);
     free(from_buf);
+    free(to_buf);
+    free(all_work);
     return 0;
 }
