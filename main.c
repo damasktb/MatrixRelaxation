e6793bc1d814e45f1b3b757f1dbeee6176b43d10
Damask Talary-Brown <damasktb@gmail.com>
2016-11-15 19:04:25 +0000

    Tidying up, commenting, fix stupid report comment because I cant subtract

--- main.c
+++ main.c
@@ -6,12 +6,6 @@
 #include <stdlib.h>
 #include <time.h>
 #include <unistd.h>
-#include "pthread_barrier.h"
-
-#define likely(x)       __builtin_expect((x),1)
-#define unlikely(x)     __builtin_expect((x),0)
-
-#define DUMMY_5x5 {1, 1, 1, 1, 1, 1, 3, 7, 2, 1, 1, 8, 6, 5, 1, 1, 9, 0, 4, 1, 1, 1, 1, 1, 1}
 
 struct args_t 
 {
@@ -35,51 +29,12 @@ typedef struct thread_work
     double precision;
 } thread_work_t;
 
-pthread_barrier_t worker_threads_done;
-pthread_barrier_t main_thread_done;
+pthread_barrier_t thread_work_barrier;
 
-int global_continue = 0;
-int global_done = 0;
-
-int minimum(int a, int b) 
-{ 
-    return a < b ? a : b; 
-}
-
-void populate_random(double **one, double **two, int nrows, int ncols) 
-{
-    srand((unsigned)time(NULL));
-    for (int i=0; i<nrows; ++i)
-    {
-        for (int j=0; j<ncols; ++j)
-        {
-            double r = 10*(double)rand()/(double)RAND_MAX;
-            one[i][j] = r;
-            two[i][j] = r;
-        }
-    }
-}
-
-void populate_values(double vals[],
-                    int nvals,
-                    double **one, 
-                    double **two, 
-                    int nrows, 
-                    int ncols)
-{
-    // Make sure we can populate the whole array from vals[]
-    assert(nvals == (nrows*ncols));
-    int v = 0;
-    for (int i=0; i<nrows; ++i)
-    {
-        for (int j=0; j<ncols; ++j)
-        {
-            one[i][j] = (double)vals[v];
-            two[i][j] = (double)vals[v];
-            ++v;
-        }
-    }
-}
+/* Set by threads which need to continue */
+volatile int global_continue = 0;
+/* Set by the main thread only, if all threads are done */
+volatile int global_done = 0;
 
 void print_matrix(double **arr, int nrows, int ncols) 
 {
@@ -94,6 +49,31 @@ void print_matrix(double **arr, int nrows, int ncols)
     printf("\n");
 }
 
+/* Populate initial arrays **one and **two identically with random floats 0-10.
+ * To generate the same numbers each time, comment out the srand() call
+ */
+void populate_random(double **one, double **two, int nrows, int ncols) 
+{
+    srand((unsigned)time(NULL));
+    for (int i=0; i<nrows; ++i)
+    {
+        for (int j=0; j<ncols; ++j)
+        {
+            double r = 10*(double)rand()/(double)RAND_MAX;
+            one[i][j] = r;
+            two[i][j] = r;
+        }
+    }
+}
+
+/* When passed references to two arrays, this will swap them */
+void swap(double ***one, double ***two)
+{
+    double **tmp = *one;
+    *one = *two;
+    *two = tmp;
+}
+
 int relax_cell(double **in, double **out, int row, int col, double precision)
 {
     double adjac = in[row-1][col]   //N
@@ -103,64 +83,72 @@ int relax_cell(double **in, double **out, int row, int col, double precision)
     double old = in[row][col];
     double new = adjac/(double)4;
     out[row][col] = new;
-    /* If the the value differences are precise enough, return 0
-     * Otherwise return 1
+    /* If the the value delta is < precision, return 0, otherwise return 1.
+     * Return values from this function can then be summed; any positive
+     * value then indicates that the relaxation is incomplete.
      */
-    // if (args.verbosity)
-    // {
-    //     printf("Delta: %f \n", fabs(old-new));
-    // }
-    return (fabs(old-new) >= precision);
+    return (precision < fabs(old-new));
 }
 
-void swap(double ***one, double ***two)
-{
-    double **tmp = *one;
-    *one = *two;
-    *two = tmp;
-}
-
+/* This is the thread work function, which will iterate until the main thread
+ * signals termination by setting global_done to 1
+ */
 void *relax_section(void *work_ptr)
 {
     thread_work_t *work = (thread_work_t*)work_ptr;
-    for (int iter=0;;++iter)
+    while (1)
     {
+        /* 0 if all cells are relaxed 'enough', 1 otherwise */
         int local_continue = 0;
         int initial_jump = 1;
+        /* We exit the loops if cells_done==work.ncells */
         int cells_done = 0;
+
         for (int i=1; i<work->nrows-1 && cells_done!=work->ncells; ++i)
         {
             for (int j=1; j<work->ncols-1 && cells_done!=work->ncells; ++j)
             {
-                if (unlikely(initial_jump))
+                /* Threads don't all start at the beginning of a row, so if it's
+                 * the first iteration, jump to the actual starting indices.
+                 * The branch predictor will catch on to this very quickly.
+                 */
+                if (initial_jump)
                 {
                     i = work->start_row;
                     j = work->start_col;
                     initial_jump = 0;
                 }
-                local_continue += relax_cell(work->read_from, work->write_to, i, j, work->precision);
+                local_continue += relax_cell(work->read_from, work->write_to, 
+                                            i, j, work->precision);
                 ++cells_done;
             }
         }
-        if (likely(local_continue))
+        if (local_continue)
         {
-            ++global_continue;
+            /* The data race here is deliberate; see report.pdf pages 4-5 */
+            global_continue = 1;
         }
-        int wait = pthread_barrier_wait(&worker_threads_done);
-        if (unlikely(wait != 0 && wait != PTHREAD_BARRIER_SERIAL_THREAD))
-        {
-            exit(-1);
-        }
-        int main_wait = pthread_barrier_wait(&main_thread_done);
-        if (unlikely(main_wait != 0 && main_wait != PTHREAD_BARRIER_SERIAL_THREAD))
+        int wait = pthread_barrier_wait(&thread_work_barrier);
+        if (wait != 0 && wait != PTHREAD_BARRIER_SERIAL_THREAD)
         {
             exit(-1);
         }
+        wait = pthread_barrier_wait(&thread_work_barrier);
+        if (wait != 0 && wait != PTHREAD_BARRIER_SERIAL_THREAD)
+        {
+            exit(-1);
+        }
+        /* If the main thread signals termination, we can return */
         if (global_done)
         {
             return NULL;
         }
-        if (work->t_id==0)print_matrix(work->write_to, work->ncols, work->nrows);
+        /* Otherwise, another iteration is required */
+        if (args.verbosity && work->t_id==0)
+        {
+            /* Print the current matrix state (from one thread only) */
+            print_matrix(work->write_to, work->ncols, work->nrows);
+        }
         swap(&work->read_from, &work->write_to);
     }
 }
@@ -177,6 +165,7 @@ void assign_thread_work(thread_work_t *all_work,
 {
     int next_col=0;
     int next_row=0;
+    /* Set the start location and number of cells to work on for each thread */
     for (int t=0; t<nthreads; ++t)
     {
         thread_work_t work = 
@@ -194,17 +183,73 @@ void assign_thread_work(thread_work_t *all_work,
         if (args.verbosity)
         {
             printf("Thread %d starting at (%d,%d) doing %d cells\n", 
-            t, 
-            work.start_row, 
-            work.start_col, 
-            work.ncells
-        );
+                t, 
+                work.start_row, 
+                work.start_col, 
+                work.ncells
+            );
+        }
         int next = next_col + (work.ncells);
         next_row += ((next) / (ncols-2));
         next_col = ((next) % (ncols-2));
         all_work[t] = work;
+    }
+}
+
+int relax_matrix(thread_work_t *work, int nthreads)
+{
+    int iter_count = 0;
+    pthread_t threads[nthreads];
+    /* Initialise the barrier to nthreads+1 since the main thread will hit
+     * both but does not do any of the relaxation work
+     */
+    pthread_barrier_init(&thread_work_barrier, NULL, nthreads+1);
+    
+    for (int t=0; t<nthreads; ++t)
+    {
+        if (pthread_create(&threads[t], NULL, relax_section, &work[t]))
+        {
+            fprintf(stderr, "Failed creating thread %d\n", t);
+            return 1;
         }
     }
+
+    while (!global_done)
+    {
+        /* Wait for all the workers to relax once, then check if we can stop. */
+        pthread_barrier_wait(&thread_work_barrier);
+        ++iter_count;
+        if (global_continue != 0)
+        {
+            /* If any thread needs to continue it sets this to 1. This needs
+             * to be reset before they iterate again.*/
+            global_continue = 0;
+        }
+        else
+        {
+            global_done = 1;
+        }
+        pthread_barrier_wait(&thread_work_barrier);
+    }
+
+    for (int t=0; t<nthreads; ++t)
+    {
+        if (pthread_join(threads[t], NULL))
+        {
+            fprintf(stderr, "Failed joining thread %d\n", t);
+            return 1;
+        }
+    }
+    
+    printf("Reached in %d iterations.\n", iter_count);
+    if (args.verbosity)
+    {
+        /* Print the finished matrix */
+        print_matrix(work[0].write_to, args.dimension, args.dimension);
+    }
+    
+    pthread_barrier_destroy(&thread_work_barrier);
+    return 0;
 }
 
 void process_options(int argc, char **argv)
@@ -234,94 +279,34 @@ void process_options(int argc, char **argv)
     }
 }
 
-int relax_matrix(thread_work_t *work, 
-                 int nthreads)
-{
-    int iter_count = 0;
-    pthread_t threads[nthreads];
-    /* Initialise the barrier to nthreads+1 since the main thread
-     * will hit both but does not count as a relaxation 'worker'
-     */
-    pthread_barrier_init(&worker_threads_done, NULL, nthreads+1);
-    pthread_barrier_init(&main_thread_done, NULL, nthreads+1);
-    
-    for (int t=0; t<nthreads; ++t)
-    {
-        if (pthread_create(&threads[t], NULL, relax_section, &work[t]))
-        {
-            fprintf(stderr, "Failed creating thread %d\n", t);
-            return 1;
-        }
-    }
-    
-    while (!global_done)
-    {
-        /* Wait for all the matrix workers to relax, then increment the
-         * iteration count and calculate the precision.
-         */
-        pthread_barrier_wait(&worker_threads_done);
-        ++iter_count;
-        if (global_continue)
-        {
-            /* If any thread needs to continue it increments this, which needs
-             * to be reset before they iterate again.*/
-            global_continue = 0;
-        }
-        else
-        {
-            global_done = 1;
-        }
-        pthread_barrier_wait(&main_thread_done);
-    }
-
-    for (int t=0; t<nthreads; ++t)
-    {
-        if (pthread_join(threads[t], NULL))
-        {
-            fprintf(stderr, "Failed joining thread %d\n", t);
-            return 1;
-        }
-    }
-    
-    if (args.verbosity)
-    {
-        printf("Reached in %d iterations.\n", iter_count);
-    }
-    
-    //print_matrix(work[0].read_from, args.dimension, args.dimension);
-    print_matrix(work[0].write_to, args.dimension, args.dimension);
-    
-    pthread_barrier_destroy(&worker_threads_done);
-    pthread_barrier_destroy(&main_thread_done);
-    return 0;
-}
-
 int main(int argc, char **argv)
 {
     /* If any of the args are invalid, fall back to these defaults */
     args.verbosity = 0;
-    args.dimension = 5;
-    args.precision = 0.75;
+    args.dimension = 100;
+    args.precision = 0.5;
     args.threads = 1;
-
     process_options(argc, argv);
 
-    int nrows = args.dimension, ncols = args.dimension;
+    /* This implmentation supports rectangular matrices */
+    int nrows = args.dimension;
+    int ncols = args.dimension;
     double precision = args.precision;
     int nthreads = args.threads;
 
+    /* Allocate an empty thread_work_t struct for each thread  */
     thread_work_t* all_work = malloc(nthreads*sizeof(thread_work_t));
-    if (unlikely(all_work==NULL))
+    if (all_work==NULL)
     {
         return 1;
     }
     
-    /* Allocate the read-from and write-to arrays */
+    /* Allocate the read-from write-to solution arrays */
     double **from = malloc(nrows*sizeof(double*));
     double **to   = malloc(nrows*sizeof(double*));
     double *from_buf = malloc(nrows*ncols*sizeof(double));
     double *to_buf   = malloc(nrows*ncols*sizeof(double));
-    if (unlikely(from==NULL || from_buf== NULL || to==NULL || to_buf== NULL))
+    if (from==NULL || from_buf== NULL || to==NULL || to_buf== NULL)
     {
         return 1;
     }
@@ -331,14 +316,10 @@ int main(int argc, char **argv)
         to[i] = to_buf + ncols*i;
     }
 
-    double dummy[25] = DUMMY_5x5;
-    populate_values(dummy, 25, from, to, nrows, ncols);
-    //populate_random(from, to, nrows, ncols);
-
-
-    /* The total number of rows to be worked on is nrows minus the first 
-     * and last. Each thread does a minimum of (nrows-2)/nthreads and a
-     * maximum of ((nrows-2)/nthreads)+1 rows, depending on the remainder.
+    populate_random(from, to, nrows, ncols);
+    /* The total number of cells to be worked on is the full matrix minus
+     * its outer values. Each thread does a minimum of cell_split and a
+     * maximum of cell_split+1 cells, depending on the remainder.
      */
     int cell_split = ((nrows-2)*(ncols-2))/nthreads;
     int cell_remainder = ((nrows-2)*(ncols-2))%nthreads;
