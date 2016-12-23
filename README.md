aebe8043392ace92d525c2504d7f46e172c154fc
Damask Talary-Brown <damasktb@gmail.com>
2016-12-23 00:23:59 +0000

    Working MPI, initial section of report, updated README

---
+++ README.md
@@ -0,0 +1,8 @@
++ Compile with: mpicc -std=gnu99 main.c -o mpi-relax
++ Run with: mpirun -np $tasks ./relax [optargs]
+
+Where [optargs] are:
+
+    -d (Integer, default=10000) The matrix dimensions    
+    -p (Double, default=0.01) The precision threshold   
+    -v (No specifier, default=false) Enable verbose mode, do not use while performance testing
--- main.c
+++ main.c
@@ -1,260 +1,67 @@
-#include <assert.h>
 #include <getopt.h>
 #include <math.h>
-#include <pthread.h>
+#include <mpi.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
-#include <unistd.h>
+
+#define ROOT_PROC 0 // The root process
 
 struct args_t 
 {
-    int verbosity;    // -v
-    int dimension;    // -d
-    double precision; // -p
-    int threads;      // -t
+    int verbosity;      // -v
+    int dimension;      // -d
+    double precision;   // -p
 } args;
 
-typedef struct thread_work
+// Determine if index i lies on an edge of a matrix with dimension nrows*ncols
+int is_edge(int nrows, int ncols, int i)
 {
-    int t_id;
-    double** read_from;
-    double** write_to;
-    int start_row;
-    int start_col;
-    int finish_row;
-    int ncells;
-    int nrows;
-    int ncols;
-    double precision;
-} thread_work_t;
+    return (i < ncols                // Top row
+        ||  i+ncols >= (nrows*ncols) // Bottom row
+        ||  i%ncols == 0             // Left edge
+        ||  (i+1)%ncols == 0 );      // Right edge
+}
 
-pthread_barrier_t thread_work_barrier;
-
-/* Set by threads which need to continue */
-volatile int global_continue = 0;
-/* Set by the main thread only, if all threads are done */
-volatile int global_done = 0;
-
-void print_matrix(double **arr, int nrows, int ncols) 
-{
-    for (int i=0; i<nrows; ++i)
+void print_matrix(double *arr, int nrows, int ncols) 
+{   
+    for (int cell=0; cell<nrows*ncols; ++cell)
     {
-        for (int j=0; j<ncols; ++j)
+        printf("%f ", arr[cell]);
+        if ((cell+1) % ncols==0) // end of a row
         {
-            printf("%f ", arr[i][j]);        
+            printf("\n"); 
         }
-        printf("\n");
     }
     printf("\n");
 }
 
-/* Populate initial arrays **one and **two identically with random floats 0-10.
- * To generate the same numbers each time, comment out the srand() call
- */
-void populate_random(double **one, double **two, int nrows, int ncols) 
+// Populate initial array with ones on the edges and zeroes in the centre
+void populate_square(double *m, int nrows, int ncols) 
 {
-    //srand((unsigned)time(NULL));
-    for (int i=0; i<nrows; ++i)
+    for (int cell=0; cell<nrows*ncols; ++cell)
     {
-        for (int j=0; j<ncols; ++j)
-        {
-            double r = 10*(double)rand()/(double)RAND_MAX;
-            one[i][j] = r;
-            two[i][j] = r;
-        }
+        m[cell] = is_edge(nrows, ncols, cell) ? 1 : 0;
     }
 }
 
-/* When passed references to two arrays, this will swap them */
-void swap(double ***one, double ***two)
+int relax_cell(double *in, double *out, int cell, int ncols, double precision)
 {
-    double **tmp = *one;
-    *one = *two;
-    *two = tmp;
-}
-
-int relax_cell(double **in, double **out, int row, int col, double precision)
-{
-    double adjac = in[row-1][col]   //N
-        + in[row+1][col]            //S
-        + in[row][col-1]            //W
-        + in[row][col+1];           //E
-    double old = in[row][col];
-    double new = adjac/(double)4;
-    out[row][col] = new;
+    double adjac = in[cell-ncols]   //N
+        + in[cell+ncols]            //S
+        + in[cell-1]                //W
+        + in[cell+1];               //E
+    double old = in[cell];
+    double new = adjac * 0.25;
+    out[cell-ncols] = new;
     /* If the the value delta is < precision, return 0, otherwise return 1.
      * Return values from this function can then be summed; any positive
-     * value then indicates that the relaxation is incomplete.
-     */
+     * value indicates that the relaxation is incomplete. */
     return (precision < fabs(old-new));
-}
-
-/* This is the thread work function, which will iterate until the main thread
- * signals termination by setting global_done to 1
- */
-void *relax_section(void *work_ptr)
-{
-    thread_work_t *work = (thread_work_t*)work_ptr;
-    while (1)
-    {
-        /* 0 if all cells are relaxed 'enough', 1 otherwise */
-        int local_continue = 0;
-        int initial_jump = 1;
-        /* We exit the loops if cells_done==work.ncells */
-        int cells_done = 0;
-
-        for (int i=1; i<work->nrows-1 && cells_done!=work->ncells; ++i)
-        {
-            for (int j=1; j<work->ncols-1 && cells_done!=work->ncells; ++j)
-            {
-                /* Threads don't all start at the beginning of a row, so if it's
-                 * the first iteration, jump to the actual starting indices.
-                 * The branch predictor will catch on to this very quickly.
-                 */
-                if (initial_jump)
-                {
-                    i = work->start_row;
-                    j = work->start_col;
-                    initial_jump = 0;
-                }
-                local_continue += relax_cell(work->read_from, work->write_to, 
-                                            i, j, work->precision);
-                ++cells_done;
-            }
-        }
-        if (local_continue)
-        {
-            /* The data race here is deliberate; see report.pdf pages 4-5 */
-            global_continue = 1;
-        }
-        int wait = pthread_barrier_wait(&thread_work_barrier);
-        if (wait != 0 && wait != PTHREAD_BARRIER_SERIAL_THREAD)
-        {
-            exit(-1);
-        }
-        wait = pthread_barrier_wait(&thread_work_barrier);
-        if (wait != 0 && wait != PTHREAD_BARRIER_SERIAL_THREAD)
-        {
-            exit(-1);
-        }
-        /* If the main thread signals termination, we can return */
-        if (global_done)
-        {
-            return NULL;
-        }
-        /* Otherwise, another iteration is required */
-        if (args.verbosity && work->t_id==0)
-        {
-            /* Print the current matrix state (from one thread only) */
-            print_matrix(work->write_to, work->ncols, work->nrows);
-        }
-        swap(&work->read_from, &work->write_to);
-    }
-}
-
-void assign_thread_work(thread_work_t *all_work,
-                        double **from,
-                        double **to,
-                        int cell_split, 
-                        int cell_remainder,
-                        int nrows,
-                        int ncols,
-                        int nthreads, 
-                        double precision)
-{
-    int next_col=0;
-    int next_row=0;
-    /* Set the start location and number of cells to work on for each thread */
-    for (int t=0; t<nthreads; ++t)
-    {
-        thread_work_t work = 
-        {
-            .t_id = t,
-            .read_from = from,
-            .write_to = to,
-            .start_row = next_row+1,
-            .start_col = next_col+1,
-            .ncells = cell_split + (t<cell_remainder ? 1:0),
-            .nrows = nrows,
-            .ncols = ncols,
-            .precision = precision,
-        };
-        if (args.verbosity)
-        {
-            printf("Thread %d starting at (%d,%d) doing %d cells\n", 
-                t, 
-                work.start_row, 
-                work.start_col, 
-                work.ncells
-            );
-        }
-        int next = next_col + (work.ncells);
-        next_row += ((next) / (ncols-2));
-        next_col = ((next) % (ncols-2));
-        all_work[t] = work;
-    }
-}
-
-int relax_matrix(thread_work_t *work, int nthreads)
-{
-    int iter_count = 0;
-    pthread_t threads[nthreads];
-    /* Initialise the barrier to nthreads+1 since the main thread will hit
-     * both but does not do any of the relaxation work
-     */
-    pthread_barrier_init(&thread_work_barrier, NULL, nthreads+1);
-    
-    for (int t=0; t<nthreads; ++t)
-    {
-        if (pthread_create(&threads[t], NULL, relax_section, &work[t]))
-        {
-            fprintf(stderr, "Failed creating thread %d\n", t);
-            return 1;
-        }
-    }
-
-    while (!global_done)
-    {
-        /* Wait for all the workers to relax once, then check if we can stop. */
-        pthread_barrier_wait(&thread_work_barrier);
-        ++iter_count;
-        if (global_continue != 0)
-        {
-            /* If any thread needs to continue it sets this to 1. This needs
-             * to be reset before they iterate again.*/
-            global_continue = 0;
-        }
-        else
-        {
-            global_done = 1;
-        }
-        pthread_barrier_wait(&thread_work_barrier);
-    }
-
-    for (int t=0; t<nthreads; ++t)
-    {
-        if (pthread_join(threads[t], NULL))
-        {
-            fprintf(stderr, "Failed joining thread %d\n", t);
-            return 1;
-        }
-    }
-    
-    printf("Reached in %d iterations.\n", iter_count);
-    if (args.verbosity)
-    {
-        /* Print the finished matrix */
-        print_matrix(work[0].write_to, args.dimension, args.dimension);
-    }
-    
-    pthread_barrier_destroy(&thread_work_barrier);
-    return 0;
 }
 
 void process_options(int argc, char **argv)
 {
-    const char *opt_string = "vd:p:t:";
+    const char *opt_string = "vd:p:t:n:";
     int opt = getopt(argc, argv, opt_string);
     while(opt != -1) 
     {
@@ -269,9 +76,6 @@ void process_options(int argc, char **argv)
             case 'p':
                 args.precision = atof(optarg);
                 break;
-            case 't':
-                args.threads = atoi(optarg);
-                break;
             default:
                 break;
         }
@@ -279,64 +83,154 @@ void process_options(int argc, char **argv)
     }
 }
 
-int main(int argc, char **argv)
+int main(int argc, char** argv)
 {
-    /* If any of the args are invalid, fall back to these defaults */
+    // If any of the args are invalid/unspecified, fall back to these defaults
     args.verbosity = 0;
-    args.dimension = 100;
-    args.precision = 0.5;
-    args.threads = 1;
+    args.dimension = 10000;
+    args.precision = 0.01;
     process_options(argc, argv);
 
-    /* This implmentation supports rectangular matrices */
-    int nrows = args.dimension;
-    int ncols = args.dimension;
-    double precision = args.precision;
-    int nthreads = args.threads;
+    // This implmentation supports rectangular matrices if you override optarg
+    const int nrows = args.dimension;
+    const int ncols = args.dimension;
+    const double precision = args.precision;
 
-    /* Allocate an empty thread_work_t struct for each thread  */
-    thread_work_t* all_work = malloc(nthreads*sizeof(thread_work_t));
-    if (all_work==NULL)
+    MPI_Init(NULL, NULL);
+    int world_rank; // The rank (id) of this process
+    MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+    int world_size; // The total number of processes
+    MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+
+    // The full matrix will only be allocated on the root process
+    double *global_matrix = NULL;
+    if (world_rank == ROOT_PROC) {
+        global_matrix = malloc(nrows*ncols*sizeof(double));
+        if (global_matrix == NULL)
+        {
+            return 1;
+        }
+        populate_square(global_matrix, nrows, ncols);
+    }
+
+    // Each process will do at least this many cells
+    const int cell_split = ncols * ((nrows-2)/world_size);  
+    // These processes will do an extra row
+    const int extra_row = (nrows-2)%world_size;
+
+    // Arrays of row counts and offsets for scattering to each process
+    int *send_counts = malloc(world_size * sizeof(int)); 
+    int *send_offset = malloc(world_size * sizeof(int)); 
+    // Arrays of counts and offsets for gathering at root from each process
+    int *rcv_counts = malloc(world_size * sizeof(int));
+    int *rcv_offset = malloc(world_size * sizeof(int));
+    if (send_counts==NULL || send_offset==NULL || rcv_counts==NULL || rcv_offset==NULL)
     {
         return 1;
     }
-    
-    /* Allocate the read-from write-to solution arrays */
-    double **from = malloc(nrows*sizeof(double*));
-    double **to   = malloc(nrows*sizeof(double*));
-    double *from_buf = malloc(nrows*ncols*sizeof(double));
-    double *to_buf   = malloc(nrows*ncols*sizeof(double));
-    if (from==NULL || from_buf== NULL || to==NULL || to_buf== NULL)
+
+    // Each process must read from the rows above and below the rows it relaxes
+    const int read_only = 2*ncols;
+    for (int proc=0; proc < world_size; ++proc)
+    {
+        /* Start at the row before where each process will start, and send the 
+         * allocated number of rows plus the two read only rows (above and below) */
+        send_counts[proc] = cell_split + (proc<extra_row? ncols : 0) + read_only;
+        send_offset[proc] = proc==0? 0 : send_offset[proc-1] + rcv_counts[proc-1];
+
+        // Receive the number of rows we sent minus the rows above and below
+        rcv_counts[proc] = send_counts[proc] - read_only;
+        // Offset starting at the row after the top boundary
+        rcv_offset[proc] = send_offset[proc] + read_only/2;
+
+        if (world_rank == 0)
+        {
+            printf("World size is %d. Process %d doing %d rows at offset %d.\n", world_size, proc, rcv_counts[proc]/ncols, rcv_offset[proc]/ncols);
+        }
+    }
+
+    /* In each process allocate a buffer that will hold a read-only subset of
+     * the matrix and a (smaller) buffer that will hold a write-only subset */
+    double *from = malloc(sizeof(double) * send_counts[world_rank]);
+    double *to   = malloc(sizeof(double) * rcv_counts[world_rank]);
+    if (from==NULL || to==NULL)
     {
         return 1;
     }
-    for (int i=0; i<nrows; ++i)
+
+    // If this process needs another iteration, it will set local_continue > 0.
+    int local_continue = 0;
+    /* Reducing MPI_SUM over local_continue for every process tells us whether
+     * we need another iteration or not. */
+    int global_continue = 0;
+
+    /* The first and last rows are read-only and will be relaxed by another
+     * process, so start at the second and finish after the penultimate. */
+    const int start = ncols;
+    const int end = start + rcv_counts[world_rank];
+    
+    for (int iters = 1; /* no exit condition - we'll break */ ;++iters)
     {
-        from[i] = from_buf + ncols*i;
-        to[i] = to_buf + ncols*i;
+        /* Scatter chunks of the array from root to each process. Use MPI_Scatterv 
+         * as the chunks will only be the same size when (nrows-2)%ntasks == 0 */
+        MPI_Scatterv(global_matrix, send_counts, send_offset, MPI_DOUBLE, 
+            from, send_counts[world_rank], MPI_DOUBLE, ROOT_PROC, MPI_COMM_WORLD);
+        
+        for (int cell=start; cell<end; ++cell)
+        {
+            /* If this is an edge cell, just copy the old fixed value.
+             * The global index of a cell is calculated as the local
+             * zero-based index plus the offset its row will be returned to. */
+            if (is_edge(nrows, ncols, rcv_offset[world_rank] + cell-start))
+            {
+                to[cell-start] = from[cell];
+            }
+            // Otherwise, relax the cell and determine if it's within precision.
+            else
+            {
+                local_continue += relax_cell(from, to, cell, ncols, precision);
+            }
+        }
+
+        // Gather the chunks back into the complete array on the root.
+        MPI_Gatherv(to, rcv_counts[world_rank], MPI_DOUBLE, global_matrix,
+            rcv_counts, rcv_offset, MPI_DOUBLE, ROOT_PROC, MPI_COMM_WORLD);
+
+        // Sum local_continue across every process to determine if we're done.
+        MPI_Allreduce(&local_continue, &global_continue, 1, MPI_INTEGER, 
+            MPI_SUM, MPI_COMM_WORLD);
+
+        if (args.verbosity && world_rank == ROOT_PROC) 
+        {
+            print_matrix(global_matrix, nrows, ncols);
+        }
+        if (global_continue)
+        {
+            local_continue = 0; // If we're not done, reset for the next loop.
+        }
+        else 
+        {
+            if (world_rank == ROOT_PROC) 
+            {
+                printf("Reached in %d iterations.\n", iters);
+            }
+            break;
+        }
     }
 
-    populate_random(from, to, nrows, ncols);
-    /* The total number of cells to be worked on is the full matrix minus
-     * its outer values. Each thread does a minimum of cell_split and a
-     * maximum of cell_split+1 cells, depending on the remainder.
-     */
-    int cell_split = ((nrows-2)*(ncols-2))/nthreads;
-    int cell_remainder = ((nrows-2)*(ncols-2))%nthreads;
-    assign_thread_work(
-        all_work, 
-        from, to, 
-        cell_split, cell_remainder, 
-        nrows, ncols, 
-        nthreads, precision
-    );
-
-    int err = relax_matrix(all_work, nthreads);
-    
+    // Tidy up and call MPI_Finalize from every process.
     free(from);
     free(to);
-    free(from_buf);
-    free(to_buf);
-    free(all_work);
-    return err;
+    if (world_rank == ROOT_PROC) 
+    {
+        free(global_matrix);
+        free(send_counts);
+        free(send_offset);
+        free(rcv_counts);
+        free(rcv_offset);
+    }
+
+    MPI_Barrier(MPI_COMM_WORLD);
+    MPI_Finalize();
+    return 0;
 }
